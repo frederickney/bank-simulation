dcce8ab9cc135cfa8078301a7b8be72494b26960
frederick.ney <frederick.ney@gmail.com>
2015-11-29 14:02:41 +0100

    attributing one banker_t to bankers

--- src/bank.c
+++ src/bank.c
@@ -8,18 +8,18 @@ void *banker (void *argv) {
 		//TODO banker
 		in_bank = 0;
 		bankers_data->current = set_current (bankers_data->tickets);
-		//printf ("current %d ", bankers_data->current);
+		printf ("current %d ", bankers_data->current);
 		for (int i = 0; i < bankers_data->nb_customers; i++) {
 			// consulting array of list_t structure for calling the right customer
 			if (bankers_data->customers_list[i].ticket == bankers_data->current) {
 				in_bank = bankers_data->customers_list[i].in_bank; // testing if the customer is in bank see next if statement
 				bankers_data->serve_customer = i; // remembering the right customer
-				//printf ("%d / ", bankers_data->serve_customer);
+				printf ("%d / ", bankers_data->serve_customer);
 				break;
 			}
 		}
 		if (in_bank) {
-			//printf ("calling %d %d\n", bankers_data->customers_list[bankers_data->serve_customer].ticket, bankers_data->serve_customer);
+			printf ("calling %d %d\n", bankers_data->customers_list[bankers_data->serve_customer].ticket, bankers_data->serve_customer);
 			sleep (bankers_data->tickets->serve_time); // serving customers
 			sem_post (&(bankers_data->customers_list[bankers_data->serve_customer].sem_customer)); // making out the customer
 			sem_wait (&(bankers_data->tickets->sem_banker)); // waiting for the next customers into list
@@ -44,7 +44,7 @@ pthread_t *create_banks (banker_t *bankers_data) {
 
 	//creating banker(s) threads
 	for (int i = 0; i < bankers_data->tickets->nb_bankers; i++)
-		if (pthread_create(bankers, NULL, banker, (void *) bankers_data) != 0) {
+		if (pthread_create(bankers, NULL, banker, (void *) &bankers_data[i]) != 0) {
 			perror ("Thread creation error");
       exit (EXIT_FAILURE);
 		}
